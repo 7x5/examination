ed884fc40d991d0846e8bb2aa3366b1a218f040a
7x5 <jacob.reinerholm@me.com>
2023-05-24 10:19:29 +0200

    lagde en måte å lagde admin bruker og for at admin bruker skal logge in

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import *
 
 import sqlite3
 
+import hashlib
+
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 
 @app.route("/")
@@ -12,6 +15,55 @@ def index():
 @app.route("/registration")
 def registration():
     return render_template("registration.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+@app.route("/register", methods=['POST'])
+def register():
+    brukernavn = request.form['brukernavn']
+    passord = request.form['passord']
+
+    hashed_passord = hashlib.sha256(passord.encode()).hexdigest()
+
+    con = sqlite3.connect('database.db')
+    c = con.cursor()
+    c.execute("INSERT INTO admin (brukernavn, passord) VALUES (?, ?)", (brukernavn, hashed_passord))
+    con.commit()
+    con.close()
+
+    return render_template("adminsuccess.html", brukernavn = brukernavn)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        brukernavn = request.form['brukernavn']
+        passord = request.form['passord']
+
+        con = sqlite3.connect('database.db')
+        c = con.cursor()
+        c.execute("SELECT * FROM admin WHERE brukernavn=?", (brukernavn,))
+        admin = c.fetchone()
+        con.close()
+
+        if admin:
+            lagret_passord = admin[2]
+            hashed_passord = hashlib.sha256(passord.encode()).hexdigest()
+            if lagret_passord == hashed_passord:
+                session['logged_in'] = True
+                return redirect(url_for('admin_dashboard')) 
+            
+        return 'Invaild password or username'
+    
+    return render_template('login.html')
+
+@app.route('/login/dashboard')
+def admin_dashboard():
+    if 'logged_in' in session and session['logged_in']:
+        return render_template('dashboard.html')
+    else:
+        return redirect(url_for('login'))
 
 @app.route("/savedetails",methods = ["POST","GET"])  
 def saveDetails():  
@@ -24,12 +76,12 @@ def saveDetails():
             epost = request.form["epost"]
             with sqlite3.connect("database.db") as con:  
                 cur = con.cursor()  
-                cur.execute("INSERT into RUSS (fornavn, etternavn, epost, telefon) values (?,?,?,?)",(fnavn,enavn,epost,telenr))  
+                cur.execute("INSERT into RUSS (fornavn, etternavn, epost, telefon, betalt) values (?,?,?,?,?)",(fnavn,enavn,epost,telenr,0))  
                 con.commit()  
-                msg = "student successfully Added"  
+                msg = "Registert, vennligst betal"  
         except:  
             con.rollback()  
-            msg = "We can not add the employee to the list"  
+            msg = "Noe gikk galt, prøv igjen om litt"  
         finally:  
             con.close()  
             return render_template("success.html",msg = msg)  
